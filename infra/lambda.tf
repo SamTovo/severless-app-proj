a47d6920d15ffa06cb67c8d5fd96001f6d947aa8
samuel.tovo <samuel.gatti.tovo98@gmail.com>
2025-02-02 20:01:58 -0300

    adicionando handler

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -12,7 +12,7 @@ resource "aws_lambda_function" "chat_lambda" {
   role          = aws_iam_role.lambda_role.arn
   s3_bucket = aws_s3_bucket.lambda.bucket
   s3_key    = "chat_lambda.zip"
-  handler = "function/chat_lambda.py"
+  handler = "chat_lambda.handler"
   runtime = "python3.8"
   source_code_hash = data.aws_s3_object.package.metadata.hash
 }
