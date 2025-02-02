5c9923fd61fb189faa0f29002d9ca32d4c356506
samuel.tovo <samuel.gatti.tovo98@gmail.com>
2025-02-02 19:58:06 -0300

    alterando paths

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -12,8 +12,7 @@ resource "aws_lambda_function" "chat_lambda" {
   role          = aws_iam_role.lambda_role.arn
   s3_bucket = aws_s3_bucket.lambda.bucket
   s3_key    = "chat_lambda.zip"
-  handler = "chat_lambda.lambda_handler"
+  handler = "function/chat_lambda.py"
   runtime = "python3.8"
   source_code_hash = data.aws_s3_object.package.metadata.hash
-
 }
