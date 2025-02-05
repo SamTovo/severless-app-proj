ceef1dfcdb5f391cff7a22e5c79422fc28f919f9
samuel.tovo <samuel.gatti.tovo98@gmail.com>
2025-02-05 11:43:59 -0300

    adicionando componentes

--- infra/api_gateway.tf
+++ infra/api_gateway.tf
@@ -71,6 +71,14 @@ resource "aws_lambda_permission" "api_gateway_post_ids" {
   source_arn    = "arn:aws:execute-api:us-east-1:311141525611:3d346uvk7h/*/POST/conversations/{id}"
 }
 
+resource "aws_lambda_permission" "api_gateway_users_get" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.user_get_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "arn:aws:execute-api:us-east-1:311141525611:3d346uvk7h/*/GET/users"
+}
+
 resource "aws_api_gateway_integration" "lambda" {
   rest_api_id             = aws_api_gateway_rest_api.api.id
   resource_id             = aws_api_gateway_resource.conversations_api.id
