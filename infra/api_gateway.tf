7888917049e7dace9b984eee02ef4d414027c732
samuel.tovo <samuel.gatti.tovo98@gmail.com>
2025-02-05 11:22:52 -0300

    adicionano api de users

--- infra/api_gateway.tf
+++ infra/api_gateway.tf
@@ -8,6 +8,11 @@ resource "aws_api_gateway_resource" "conversations_api" {
   parent_id   = aws_api_gateway_rest_api.api.root_resource_id
   path_part   = "conversations"
 }
+resource "aws_api_gateway_resource" "users_api" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "users"
+}
 resource "aws_api_gateway_resource" "conversations_id_api" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   parent_id   = aws_api_gateway_resource.conversations_api.id
@@ -33,6 +38,12 @@ resource "aws_api_gateway_method" "post_ids" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
   resource_id   = aws_api_gateway_resource.conversations_id_api.id
   http_method   = "POST"
+  authorization = "NONE"
+}
+resource "aws_api_gateway_method" "get_users" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.users_api.id
+  http_method   = "GET"
   authorization = "NONE"
 }
 
@@ -101,6 +112,18 @@ resource "aws_api_gateway_integration" "lambda_post_ids" {
   passthrough_behavior = "WHEN_NO_TEMPLATES"
 }
 
+resource "aws_api_gateway_integration" "lambda_users_get" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.users_api.id
+  http_method             = aws_api_gateway_method.get_users.http_method
+  integration_http_method = "POST"
+  type                    = "AWS"
+  uri                     = aws_lambda_function.user_get_lambda.invoke_arn
+  #   request_templates = {
+  #   "application/json" = file("${path.module}/mapping_templates/Chat-Messages-POST-Input.vtl")
+  # }
+  # passthrough_behavior = "WHEN_NO_TEMPLATES"
+}
 
 resource "aws_api_gateway_model" "conversation_list" {
   rest_api_id  = aws_api_gateway_rest_api.api.id
@@ -123,6 +146,12 @@ resource "aws_api_gateway_model" "new_message" {
   schema       = file("${path.module}/models/NewMessage.json")
 }
 
+resource "aws_api_gateway_model" "user_get" {
+  rest_api_id  = aws_api_gateway_rest_api.api.id
+  name         = "UserList"
+  content_type = "application/json"
+  schema       = file("${path.module}/models/UserList.json")
+}
 
 resource "aws_api_gateway_method_response" "method_response_200" {
   rest_api_id = aws_api_gateway_rest_api.api.id
@@ -147,6 +176,33 @@ resource "aws_api_gateway_integration_response" "integration_response_200" {
     "method.response.header.Access-Control-Allow-Origin" = "'*'"
   }
 }
+
+
+
+resource "aws_api_gateway_method_response" "method_response_users_200" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.users_api.id
+  http_method = aws_api_gateway_method.get_users.http_method
+  status_code = "200"
+  response_models = {
+    "application/json" = aws_api_gateway_model.user_get.name
+  }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+}
+
+resource "aws_api_gateway_integration_response" "integration_response_users_200" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.users_api.id
+  http_method = aws_api_gateway_method.get_users.http_method
+  status_code = aws_api_gateway_method_response.method_response_users_200.status_code
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+  }
+}
+
 
 
 resource "aws_api_gateway_method_response" "method_response_200_id_get" {
@@ -286,6 +342,56 @@ resource "aws_api_gateway_method_response" "cors_method_response_id" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   resource_id = aws_api_gateway_resource.conversations_id_api.id
   http_method = aws_api_gateway_method.cors_options_id.http_method
+  status_code = "200"
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
+
+
+resource "aws_api_gateway_method" "cors_users_options" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.users_api.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "cors_users_integration" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.users_api.id
+  http_method = aws_api_gateway_method.cors_options.http_method
+  type        = "MOCK"
+
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "cors_users_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.users_api.id
+  http_method = aws_api_gateway_method.cors_options.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent'"
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+}
+
+resource "aws_api_gateway_method_response" "cors_users_method_response" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.users_api.id
+  http_method = aws_api_gateway_method.cors_options.http_method
   status_code = "200"
 
   response_models = {
